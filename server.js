c2e94e372cb8e7a167f787013a5b295876591fd8
Kalashkg153 <kalashkg310@gmail.com>
2022-07-18 13:54:57 +0530

    second add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 
 const path = require('node:path');
+const cors = require('cors');
 
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,11 @@ app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
+
+const corsOptions ={
+    origin : process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
